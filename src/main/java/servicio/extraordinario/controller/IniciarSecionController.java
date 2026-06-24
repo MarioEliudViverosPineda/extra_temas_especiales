b66e73c73bc1ef1bf3976281bec4ccd485a9d113
Mario Viveros Pineda <meviverosp_becario@consultoria-ultrasist.com.mx>
2026-06-23 21:58:52 -0600

    Documentacion

--- src/main/java/servicio/extraordinario/controller/IniciarSecionController.java
+++ src/main/java/servicio/extraordinario/controller/IniciarSecionController.java
@@ -14,16 +14,34 @@ public class IniciarSecionController {
 
     private final UsuarioService usuarioService;
 
+    /**
+     * Servicio que permite ir a la vista de loging.
+     *
+     * @return
+     */
     @GetMapping("/login")
     public String mostrarLogin() {
         return "usuario/loging";
     }
 
+    /**
+     * Servicio que permite ingresar al registro.
+     *
+     * @return
+     */
     @GetMapping("/registro")
     public String mostrarRegistro() {
         return "usuario/registro";
     }
 
+    /**
+     * Servicio que permite loggear con el usuario registrado.
+     *
+     * @param correo
+     * @param contrasena
+     * @param model
+     * @return
+     */
     @PostMapping("/login")
     public String login(@RequestParam String correo,
                         @RequestParam String contrasena,
@@ -37,6 +55,13 @@ public class IniciarSecionController {
         }
     }
 
+    /**
+     * Servicio que permite regitrar al usuario.
+     *
+     * @param usuario
+     * @param model
+     * @return
+     */
     @PostMapping("/registro")
     public String registrar(@ModelAttribute PersonaEntity usuario, Model model) {
         try {
